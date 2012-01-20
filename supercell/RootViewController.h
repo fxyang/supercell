a78bde40311d3011e66cdd4fb863066b0be6270b
fxyang <fxyang@gmail.com>
2012-01-20 11:16:51 -0800

    Initial

---
+++ supercell/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  supercell
+//
+//  Created by Feixue Yang on 12-01-18.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
